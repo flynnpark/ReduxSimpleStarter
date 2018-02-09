4bbe8823792f82d19c20bf7ba16900afb184f96d
Flynn <wphestiraid@gmail.com>
2018-02-09 12:47:57 +0900

    7. JSX와의 첫만남
    - const에 할당된 변수는 재할당할 수 없다.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Create a new component. This component shuld produce some html
+
+const App = () => { // const는 재할당 하지 못함
+    return <div>Hi!</div>;
+}
+
+// Take this component's generated HTML and put it on the page
+// (in the DOM)
