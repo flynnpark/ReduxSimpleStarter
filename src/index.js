f94d89aa055519924bda73f00866670d3602a2bd
Flynn <wphestiraid@gmail.com>
2018-02-12 12:41:49 +0900

    리액트DOM vs 리액트

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 // Create a new component. This component shuld produce some html
 
@@ -9,4 +10,4 @@ const App = () => { // const는 재할당 하지 못함
 // Take this component's generated HTML and put it on the page
 // (in the DOM)
 
-React.render(App);
+ReactDOM.render(App);
