afa941ecc051d3d3de8d3cad8f981f405389c331
Flynn <wphestiraid@gmail.com>
2018-02-22 13:01:26 +0900

    제어 컴포넌트
    - 누락 코드

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,7 +10,12 @@ class SearchBar extends Component {
     render() {
         return (
             <div>
-                <input value={this.state.term} />
+                <input
+                    value={this.state.term}
+                    onChange={event =>
+                        this.setState({ term: event.target.value })
+                    }
+                />
             </div>
         );
     }
