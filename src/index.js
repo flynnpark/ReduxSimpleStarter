7f5afe46be03d5f31f202cb395f979e56a99833f
Flynn <wphestiraid@gmail.com>
2018-02-26 12:46:05 +0900

    함수형 컴포넌트를 클래스 컴포넌트로 리팩토링하기
    - 컴포넌트를 초기화하면서 상태를 초기화함

--- src/index.js
+++ src/index.js
@@ -1,25 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import { API_KEY } from '../config.js';
 import SearchBar from './components/search_bar';
 
-YTSearch({ key: API_KEY, term: 'samsung' }, data => {
-    console.log(data);
-});
-
 // Create a new component. This component shuld produce some html
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-const App = () => {
-    // const는 재할당 하지 못함
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
-};
+        this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: 'samsung' }, videos => {
+            this.setState({ videos });
+        });
+    }
+    render() {
+        // const는 재할당 하지 못함
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
+}
 
 // Take this component's generated HTML and put it on the page
 // (in the DOM)
-
 ReactDOM.render(<App />, document.querySelector('.container'));
