e30d393db4dbbc69e219b03ca4bbf8260beaac60
Flynn <wphestiraid@gmail.com>
2018-03-08 12:38:03 +0900

    컨테이너 클래스 구현
    - 리듀서 오타 수정
    - book-list 컨테이너에서 mapStateToProps 사용하여 state를 props로 연결

--- ReduxSimpleStarter/src/containers/book-list.js
+++ ReduxSimpleStarter/src/containers/book-list.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class BookList extends Component {
     renderList() {
@@ -16,4 +17,10 @@ class BookList extends Component {
     }
 }
 
-export default BookList;
+function mapStateToProps(state) {
+    return {
+        books: state.books
+    };
+}
+
+export default connect(mapStateToProps)(BookList);
