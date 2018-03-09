f3da300c32daa466f654f6ec9a3fad252300071a
Flynn <wphestiraid@gmail.com>
2018-03-09 12:51:00 +0900

    액션과 액션 생성자

--- ReduxSimpleStarter/src/containers/book-list.js
+++ ReduxSimpleStarter/src/containers/book-list.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
     renderList() {
@@ -23,4 +25,15 @@ function mapStateToProps(state) {
     };
 }
 
-export default connect(mapStateToProps)(BookList);
+// Anything returned from this function will end up as props
+// on the BookList container
+const mapDispatchToProps = dispatch => {
+    // Whenever selectBook is called, the result shuld be passed
+    // to all of our reducers
+    return bindActionCreators({ selectBook }, dispatch);
+};
+
+// Promote BookList from a component to a container - it needs to know
+// about this new dispatch method, selecttBook. Make it available
+// as a prop.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
