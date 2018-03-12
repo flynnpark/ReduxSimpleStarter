1650016973ba3d1fb412f227c6847ddc40c61ee7
Flynn <wphestiraid@gmail.com>
2018-03-12 12:50:09 +0900

    리듀서 안의 액션 소비하기 2

---
+++ ReduxSimpleStarter/src/containers/book-detail.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class BookDetail extends Component {
+    render() {
+        return <div>Book Detail</div>;
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        book: state.activeBook
+    };
+};
+
+export default connect(mapStateToProps)(BookDetail);
