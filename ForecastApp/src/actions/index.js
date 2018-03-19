54597e75e972e1f54e91710b7f882c51a443766b
Flynn <wphestiraid@gmail.com>
2018-03-19 12:39:16 +0900

    Axios로 Ajax 리퀘스트
    - Axios 설치

--- ForecastApp/src/actions/index.js
+++ ForecastApp/src/actions/index.js
@@ -1,1 +1,15 @@
+import axios from 'axios';
+
 const API_KEY = '407ced4aac717eb32736b8da7c3fd71f';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},kr`;
+    const request = axios.get(url);
+    return {
+        type: FETCH_WEATHER'
+        payload: request
+    };
+}
