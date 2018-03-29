70abbc20b4aec636ea2d2f4fecc93826f0aeee9a
Flynn <wphestiraid@gmail.com>
2018-03-29 12:34:38 +0900

    리액트 라우터 세팅

--- RouterAndForm/src/index.js
+++ RouterAndForm/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
+import { Router, browserHistory } from 'react-router';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
@@ -11,5 +11,6 @@ const createStoreWithMiddleware = applyMiddleware()(createStore);
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
-  </Provider>
-  , document.querySelector('.container'));
+  </Provider>,
+  document.querySelector('.container')
+);
