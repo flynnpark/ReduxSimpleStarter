2accd457e410fafe3099db0b185653a76b85390e
Flynn <wphestiraid@gmail.com>
2018-03-29 12:51:45 +0900

    중첩된 라우트
    - 중첩된 라우트의 경우 한 라우트 안에 다른 라우트가 있는 구조
    - 복잡한 URL을 정의할 수 있음
    - 부모 컴포넌트의 props.children으로 라우트가 들어감

--- RouterAndForm/src/routes.js
+++ RouterAndForm/src/routes.js
@@ -3,4 +3,14 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
 
-export default <Route path="/" component={App} />;
+const Greeting = () => {
+  return <div>Hey there!</div>;
+};
+
+export default (
+  <Route path="/" component={App}>
+    <Route path="greet" component={Greeting} />
+    <Route path="greet2" component={Greeting} />
+    <Route path="greet3" component={Greeting} />
+  </Route>
+);
