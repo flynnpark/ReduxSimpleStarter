a2bf430cb50a7640b7158a4a0f57ad4bd5413f5f
Flynn <wphestiraid@gmail.com>
2018-04-06 12:43:41 +0900

    CreatePost 액션 생성자

--- RouterAndForm/src/components/posts_new.js
+++ RouterAndForm/src/components/posts_new.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
   render() {
     const { fields: { title, categories, content }, handleSubmit } = this.props;
 
     return (
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Create A New Post</h3>
         <div className="form-group">
           <label>Title</label>
@@ -28,7 +29,13 @@ class PostsNew extends Component {
   }
 }
 
-export default reduxForm({
-  form: 'PostsNewForm',
-  fields: ['title', 'categories', 'content']
-})(PostsNew);
+// reduxForm: 1st is form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps
+
+export default reduxForm(
+  {
+    form: 'PostsNewForm',
+    fields: ['title', 'categories', 'content']
+  },
+  null,
+  { createPost }
+)(PostsNew);
