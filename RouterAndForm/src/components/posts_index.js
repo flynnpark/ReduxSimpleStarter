c364ad8fa6b8d6a6c2bb3a7ff5c5cae8048b4481
Flynn <wphestiraid@gmail.com>
2018-04-16 12:45:41 +0900

    포스트 인덱스

--- RouterAndForm/src/components/posts_index.js
+++ RouterAndForm/src/components/posts_index.js
@@ -8,6 +8,17 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    return this.props.posts.map(post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          <span className="pull-xs-right">{post.categories}</span>
+          <strong>{post.title}</strong>
+        </li>
+      );
+    });
+  }
+
   render() {
     return (
       <div>
@@ -15,10 +26,16 @@ class PostsIndex extends Component {
           <Link to="/posts/new" className="btn btn-primary">
             Add a post
           </Link>
-        </div>List of blog posts
+        </div>
+        <h3>Posts</h3>
+        <ul className="list-group">{this.renderPosts()}</ul>
       </div>
     );
   }
 }
 
-export default connect(null, { fetchPosts: fetchPosts })(PostsIndex);
+const mapStateToProps = state => {
+  return { posts: state.posts.all };
+};
+
+export default connect(mapStateToProps, { fetchPosts: fetchPosts })(PostsIndex);
